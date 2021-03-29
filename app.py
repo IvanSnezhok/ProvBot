94971915fc58d6e898ac55c596276591965bee1e
ivansnezhok <ivan.snezhok@gmail.com>
2021-03-29 15:21:57 +0300

    Добавил уведомление об отключении

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from aiogram import executor
 
 from loader import dp
-import middlewares, filters, handlers
-from utils.notify_admins import on_startup_notify
+from utils.notify_admins import on_startup_notify, on_shutdown_notify
+
 
 async def on_startup(dispatcher):
     # Уведомляет про запуск
@@ -10,4 +10,4 @@ async def on_startup(dispatcher):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown_notify)

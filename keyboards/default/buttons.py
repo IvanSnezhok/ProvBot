6999c5027d6d7114576e552b9d67585199f67380
ivansnezhok <ivan.snezhok@gmail.com>
2021-03-30 13:32:33 +0300

    Теперь можно получить номер клиента который общаеться с ботом

---
+++ keyboards/default/buttons.py
@@ -0,0 +1,8 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+tel_button = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
+    [
+        KeyboardButton(text="📱",
+                       request_contact=True)
+    ]
+],one_time_keyboard=True)

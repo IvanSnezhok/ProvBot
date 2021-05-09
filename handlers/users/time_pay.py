527cf4edb5221bf8071e1ef53914f9904e32782a
ivan.snezhok@gmail.com <Velebnost_133768>
2021-05-09 16:01:51 +0300

    Лог сообщений с временного платежа

--- handlers/users/time_pay.py
+++ handlers/users/time_pay.py
@@ -9,11 +9,14 @@ from utils.db_api import database
 
 @dp.message_handler(Text(equals=__("Тимчасовий платіж")))
 async def time_pay(message: types.Message):
+    await db.message(message.from_user.full_name, message.from_user.id, message.text, message.date)
     if await database.t_pay(await db.select_contract(message.from_user.id)):
-        await message.answer(text=_("Доступ в Інтернет розблоковано на 24 години!\n"
-                                    "Тепер можете повернутись у головне меню"),
-                             reply_markup=return_button)
+        msg = await message.answer(text=_("Доступ в Інтернет розблоковано на 24 години!\n"
+                                          "Тепер можете повернутись у головне меню"),
+                                   reply_markup=return_button)
+        await db.message("BOT", 10001, msg.html_text, msg.date)
     else:
-        await message.answer(text=_("Ви не можете зробити тимчасовий платіж зараз!\n"
-                                    "Користуватись тимчасвовим платежом можна тільки раз на місяць!"),
-                             reply_markup=return_button)
+        msg = await message.answer(text=_("Ви не можете зробити тимчасовий платіж зараз!\n"
+                                          "Користуватись тимчасвовим платежом можна тільки раз на місяць!"),
+                                   reply_markup=return_button)
+        await db.message("BOT", 10001, msg.html_text, msg.date)
